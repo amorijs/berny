1784a06a10f1e24803b40b7c172afe4efb595ccc
Chris Amori <chris.amori93@gmail.com>
2024-05-05 14:58:41 -0700

    Landing page updates

--- src/app/_components/Landing.tsx
+++ src/app/_components/Landing.tsx
@@ -2,26 +2,32 @@ import Image from 'next/image'
 
 export async function Landing() {
   return (
-    <div>
-      <main className="container mx-auto px-4 py-12">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-          <div className="md:col-span-1">
-            <h1 className="mb-4 text-4xl font-bold">Berny</h1>
-            <p className="mb-6">
-              Stop giving away your email. Let Berny handle it for you.
-            </p>
-          </div>
-          <div className="md:col-span-1">
-            <Image
-              width={500}
-              height={500}
-              src="/berny-transparentfull.png"
-              alt="Illustration"
-              className="h-auto w-full"
-            />
-          </div>
-        </div>
-      </main>
+    <div className="p-20">
+      <div>
+        <h1 className="mb-4 text-4xl font-bold">Berny</h1>
+        <p className="mb-6">
+          Stop giving away your email. Let Berny handle it for you.
+        </p>
+        <p>
+          Berny generates emails for you on the fly. All of these emails route
+          back to your primary email.
+        </p>
+        <Image
+          width={900}
+          height={576}
+          src="/emailexample2.png"
+          alt="email-example"
+        />
+        <p className="mt-8">This landing page is awesome, I know.</p>
+        <p>
+          Feel free to fork and deploy your own instances of Berny for personal
+          use. But please don&apos;t use it for any commercial purposes.
+        </p>
+        <p>
+          PS, Berny&apos;s MIT license was created by Copilot, so its
+          bulletproof. Good luck in court.
+        </p>
+      </div>
 
       <footer className="py-4">
         <div className="container mx-auto px-4">
